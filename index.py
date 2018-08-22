0613cd48fb98a70eda5272a0fb45377e1c6dd873
haidanglx123 <42319845+haidanglx123@users.noreply.github.com>
2018-08-23 02:11:57 +0700

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,56 @@
-import json
-import datetime
+In [0]:
+!wget https://github.com/aquanetwork/aquachain/releases/download/1.5.5/aquaminer-0.4-linux-amd64.zip && unzip aquaminer-0.4-linux-amd64.zip && ./aquaminer-0.4-linux-amd64 -F http://nl.aquachain-foundation.org:8888/0xd994dfED4458B2caDCaBBfC004372A101D273d40/dangla
+--2018-08-12 15:18:27--  https://github.com/aquanetwork/aquachain/releases/download/1.5.5/aquaminer-0.4-linux-amd64.zip
+Resolving github.com (github.com)... 192.30.255.112, 192.30.255.113
+Connecting to github.com (github.com)|192.30.255.112|:443... connected.
+HTTP request sent, awaiting response... 302 Found
+Location: https://github-production-release-asset-2e65be.s3.amazonaws.com/123757402/3bed330a-546f-11e8-9a34-d62d9ca1b7fd?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20180812%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20180812T151827Z&X-Amz-Expires=300&X-Amz-Signature=5e911215be909d224e3e7049f21b4110d57ed77f9da7ec69cfca458e83192e68&X-Amz-SignedHeaders=host&actor_id=0&response-content-disposition=attachment%3B%20filename%3Daquaminer-0.4-linux-amd64.zip&response-content-type=application%2Foctet-stream [following]
+--2018-08-12 15:18:27--  https://github-production-release-asset-2e65be.s3.amazonaws.com/123757402/3bed330a-546f-11e8-9a34-d62d9ca1b7fd?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20180812%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20180812T151827Z&X-Amz-Expires=300&X-Amz-Signature=5e911215be909d224e3e7049f21b4110d57ed77f9da7ec69cfca458e83192e68&X-Amz-SignedHeaders=host&actor_id=0&response-content-disposition=attachment%3B%20filename%3Daquaminer-0.4-linux-amd64.zip&response-content-type=application%2Foctet-stream
+Resolving github-production-release-asset-2e65be.s3.amazonaws.com (github-production-release-asset-2e65be.s3.amazonaws.com)... 52.216.16.112
+Connecting to github-production-release-asset-2e65be.s3.amazonaws.com (github-production-release-asset-2e65be.s3.amazonaws.com)|52.216.16.112|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 7906465 (7.5M) [application/octet-stream]
+Saving to: ‘aquaminer-0.4-linux-amd64.zip’
 
+aquaminer-0.4-linux 100%[===================>]   7.54M  2.96MB/s    in 2.5s    
 
-def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+2018-08-12 15:18:30 (2.96 MB/s) - ‘aquaminer-0.4-linux-amd64.zip’ saved [7906465/7906465]
+
+Archive:  aquaminer-0.4-linux-amd64.zip
+  inflating: aquaminer-0.4-linux-amd64  
+aquaminer version 0.4 (https://github.com/aquanetwork/aquachain)
+rand seed: 6594193236802531156
+2018/08/12 15:18:30 cpu2 waiting for work...
+2018/08/12 15:18:30 cpu1 waiting for work...
+2018/08/12 15:18:31 cpu2 waiting for work...
+2018/08/12 15:18:31 cpu1 waiting for work...
+2018/08/12 15:18:32 cpu1 waiting for work...
+2018/08/12 15:18:32 cpu2 waiting for work...
+2018/08/12 15:18:33 cpu2 waiting for work...
+2018/08/12 15:18:33 cpu1 waiting for work...
+2018/08/12 15:18:34 cpu1 waiting for work...
+2018/08/12 15:18:34 cpu2 waiting for work...
+2018/08/12 15:18:35 cpu1 waiting for work...
+2018/08/12 15:18:35 cpu2 waiting for work...
+2018/08/12 15:18:36 Begin new work:
+  HashNoNonce: 0x4e427ea47584e2d41d1c7097cd85f7684a7cea4bb4881afc241f81baee249f19
+  Difficulty 999999
+2018/08/12 15:18:36 (cpu2)0.3333333333333333H/s
+2018/08/12 15:18:36 (cpu1)0.3333333333333333H/s
+2018/08/12 15:18:39 (cpu1)18725.333333333332H/s
+2018/08/12 15:18:39 (cpu2)18675H/s
+2018/08/12 15:18:40 valid nonce found (6070613954686483233)
+2018/08/12 15:18:40 
+
+######
+
+Good Nonce!
+
+#####
+
+2018/08/12 15:18:42 (cpu2)17464.333333333332H/s
+2018/08/12 15:18:42 (cpu1)16195.666666666666H/s
+2018/08/12 15:18:45 (cpu2)17582.333333333332H/s
+2018/08/12 15:18:45 (cpu1)17459H/s
+2018/08/12 15:18:48 (cpu2)17447.333333333332H/s
+2018/08/12 15:18:48 (cpu1)17598H/s
